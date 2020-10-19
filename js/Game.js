be81c36f725a0555a17510b9dc2b4b2c76f4d7b8
zaid <zaidbinhamid23@gmail.com>
2020-10-19 16:34:02 +0530

    adding reset button and player indicator

--- js/Game.js
+++ js/Game.js
@@ -73,6 +73,9 @@ class Game {
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
+          stroke(10);
+          fill("red");
+          ellipse(x,y,70,70);
         }
        
         //textSize(15);
